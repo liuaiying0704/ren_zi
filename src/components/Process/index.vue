089d8caf02726a217c13a40adb698246a63c63bd
liuaiying <1425577366@qq.com>
2022-08-13 14:19:48 +0800

    按钮封装

---
+++ src/components/Process/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- <div :class="[type, size]" class="btn"> -->
+  <div class="process">
+    <div></div>
+  </div>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  // props: {
+  //   type: {
+  //     type: String,
+  //     default: 'default',
+  //   },
+  //   size: {
+  //     type: String,
+  //     default: 'large',
+  //   },
+  // },
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+.process {
+  width: 100px;
+  height: 20px;
+  background-color: #eee;
+}
+</style>
